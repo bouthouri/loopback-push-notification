f06a778781f46d43be880c5b54329ce24b742d0e
Oussama Bouthouri <oussama@bouthouri.com>
2019-09-27 11:32:24 +0100

    add push to all users logic

--- common/models/message.js
+++ common/models/message.js
@@ -3,11 +3,34 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+var OneSignal = require("onesignal-node");
+
 module.exports = function(Message) {
-  Message.greet = function(msg, cb) {
-    process.nextTick(function() {
-      msg = msg || 'hello';
-      cb(null, 'Sender says ' + msg + ' to receiver');
+  Message.greet = function(cb) {
+    var myClient = new OneSignal.Client({
+      userAuthKey: "OGE0MzUyNjAtMjM4Ny00NjNhLTk5NjctMDhmN2EwOTE3NjJm",
+      app: {
+        appAuthKey: "OGE0MzUyNjAtMjM4Ny00NjNhLTk5NjctMDhmN2EwOTE3NjJm",
+        appId: "fd568c3d-4d40-405f-9214-b5acf470fac8"
+      }
+    });
+
+    // we need to create a notification to send
+    var firstNotification = new OneSignal.Notification({
+      contents: { en: "Hello dear user" },
+      included_segments: ["Subscribed Users"]
+    });
+
+    myClient.sendNotification(firstNotification, function(
+      err,
+      httpResponse,
+      data
+    ) {
+      if (err) {
+        console.log("Something went wrong...");
+      } else {
+        cb(null, "The notification was sent");
+      }
     });
   };
 };
